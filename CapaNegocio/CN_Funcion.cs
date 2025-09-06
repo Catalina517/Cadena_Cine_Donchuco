aca868d7dc9aa59acc172fc2aca413ae4de572ea
Johan Alexander Alzate Perez <johan.alzate678@pascualbravo.edu.co>
2025-09-06 13:37:42 -0500

    Cambio Cn_Funcion

--- CapaNegocio/CN_Funcion.cs
+++ CapaNegocio/CN_Funcion.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using CapaDatos;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,28 @@ namespace CapaNegocio
 {
     public class CN_Funcion
     {
+        private CD_Funcion objCd= new CD_Funcion();
+
+
+
+
+        public void RegistarF(int codigo, DateTime fechaHora, string sala) 
+        {
+          objCd.RegistrarFuncion(codigo, fechaHora, sala);
+        }
+
+
+        public DataTable ListarF()
+        {
+            return objCd.ListarFunciones();
+            
+        }
+
+        public DataRow BuscarF (int codigo) 
+        {
+          return objCd.BuscarFuncion(codigo);
+        }
+
+
     }
 }
