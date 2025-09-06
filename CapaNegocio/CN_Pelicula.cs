ee2396ec058d53174c2d6429bbc1f43a6db62b27
Catalina <catalina.correa346@pascualbravo.edu.co>
2025-09-06 13:37:55 -0500

    CN PELICULA Y CN VENTAS

--- CapaNegocio/CN_Pelicula.cs
+++ CapaNegocio/CN_Pelicula.cs
@@ -3,10 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CapaDatos;
+using System.Data;
 
 namespace CapaNegocio
 {
     public class CN_Pelicula
     {
+        private CD_Pelicula objetoCD = new CD_Pelicula();
+
+        // Método para registrar película
+        public void RegistrarPelicula(int codigo, string nombre, int duracion, string genero)
+        {
+            objetoCD.RegistrarPelicula(codigo, nombre, duracion, genero);
+        }
+
+        // Método para buscar película por código
+        public DataRow BuscarPelicula(int codigo)
+        {
+            return objetoCD.BuscarPelicula(codigo);
+        }
     }
 }
--- CapaNegocio/CN_Ventas.cs
+++ CapaNegocio/CN_Ventas.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CapaDatos;
+using System.Data;
+
 
 namespace CapaNegocio
 {
     public class CN_Ventas
     {
+        private CD_Ventas objetoCD = new CD_Ventas();
+
+        //precio de boletas
+        private const decimal PrecioBoleta = 12000m;
+        // Método para registrar venta
+        public void RegistrarVenta(int idFuncion, int cantidadBoletas)
+        {
+            decimal valorTotal = cantidadBoletas * PrecioBoleta;
+            objetoCD.RegistrarVenta(idFuncion, cantidadBoletas, valorTotal);
+        }
     }
 }
